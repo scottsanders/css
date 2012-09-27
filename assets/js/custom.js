62bcc028100cb86de7a77d598bfd52eacf3d87cd
Scott Sanders <scottsanders87@gmail.com>
2012-09-28 08:23:52 +1000

    Added customisable layers with basic CSS inputs.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,23 +3,33 @@
   var layers = $("#layers");
   var canvas = $("#canvas");
 
+  var current_layer = false;
+
   $("#add").click(function(){
   	
   	next = layers.find("li").size() + 1;
   	
-  	layers.append("<li><a href=\"#\" rel=\"layer" + next+ "\">Layer " + next + "</a></li>").find("a").popover({
+  	layers.append("<li><a href=\"#\" rel=\"layer" + next+ "\"><span id=\"sample" + next +"\"></span>Layer " + next + "</a></li>").find("a").popover({
 	  	animation: false,
 	  	title: "Layer Options",
 	  	trigger: "manual",
+      content: $("#form").html()
 	});
-	canvas.append("<div id=\"layer" + next + "\">" + next + "</div>");
+	canvas.append("<div id=\"layer" + next + "\" class=\"layer\">" + next + "</div>");
 
   });
 
   $(layers).on("click","a",function(){
+    current_layer = false;
   	layers.find("a[rel!=" + $(this).attr("rel")+ "]").popover("hide").parent().removeClass("active");	
   	$(this).popover("toggle").parent().toggleClass("active");	
+    if($(this).parent().hasClass("active")) current_layer = $("#"+$(this).attr("rel"));
   	return false;
+  });
+
+  $(document).on("keyup","form input",function(){
+    console.log($(this).attr("rel")+", "+$(this).val());
+    current_layer.css($(this).attr("rel"), $(this).val());
   });
 
 }(window.jQuery);
